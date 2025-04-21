9fd47447455b8d2b159b53ab9a62f8a79169b7c8
Jana Tomastikova <j.tomastikova@sms.ed.ac.uk>
2025-04-21 23:02:13 +0100

    ed

--- R/ed.R
+++ R/ed.R
@@ -5,17 +5,22 @@ library(audio)
 ed <- function(x) {
   # Locate the sound files using system.file
   hungry <- system.file("extdata", "Im_so_hungry.wav", package = "quoted")
+  scary <- system.file("extdata", "Scary.wav", package = "quoted")
 
 
   # Load the wave files
   ed_hungry <- load.wave(hungry)
+  ed_scary <- load.wave(scary)
 
 
   # Play the selected sound
   if (x == 1)
     play(ed_hungry)
+  if (x == 2)
+    play(ed_scary)
 
 }
 
 # Example usage
 ed(1)
+ed(2)
